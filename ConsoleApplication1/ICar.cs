0c798b40db3c6020975a6e13306a251e519e3d99
yaroslav <yaroslav.golyuk@yandex.ua>
2017-02-17 14:36:53 +0200

    Add project files.

---
+++ ConsoleApplication1/ICar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public interface ICar
+    {
+        ISize ISize { get; set; }
+        double Price { get; set; }
+        double CountPrice(List<IBox> Boxes);
+    }
+}
---
+++ ConsoleApplication1/ISize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public interface ISize
+    {
+        double Width { get; set; }
+        double Height { get; set; }
+        double Length { get; set; }
+    }
+}
