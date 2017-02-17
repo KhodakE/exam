8735d3e130d896c3711a18d618b0153e31235365
Pavel <pavelrippa@gmail.com>
2017-02-17 15:01:30 +0200

    1

---
+++ ClassLibrary1/BoxTypes/BoxCarton.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApplication1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.BoxTypes
+{
+    class BoxCarton : IBox
+    {
+        public ISize ISize
+        {
+            get
+            {
+                return new Size();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
