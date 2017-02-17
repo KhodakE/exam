2f318ebac6651c469c93d33576a0856a68b5f818
yaroslav <yaroslav.golyuk@yandex.ua>
2017-02-17 15:18:04 +0200

    update

---
+++ ConsoleApplication1/Car/SmallCar.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApplication1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Car
+{
+    class SmallCar : ICar
+    {
+        public ISize ISize
+        {
+            get
+            {
+                return new ConsoleApplication1.Size.Size();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return 200;
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public double CountPrice(List<IBox> Boxes)
+        {
+            //double len = Boxes.Sum(s => s.ISize.Length);
+            throw new NotImplementedException();
+        }
+    }
+}
