8a53f817709ebf31c82daf34f82db731a02d5eee
Melomen <melomen.one@gmail.com>
2017-02-17 16:28:24 +0200

    resolved public

--- ConsoleApplication1/Size/SizeComparer.cs
+++ ConsoleApplication1/Size/SizeComparer.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1.Size
 {
-    class SizeComparer
+    public class SizeComparer
     {
-        bool Compare(ISize sizeA, ISize sizeB)
+        public bool Compare(ISize sizeA, ISize sizeB)
         {
             if (sizeA.Height > sizeB.Height && sizeA.Length > sizeB.Length && sizeA.Width > sizeB.Width)
                 return true;
@@ -17,7 +17,7 @@ namespace ConsoleApplication1.Size
                 return false;
         }
 
-        bool CompareVolume(SizeVolume volumeA, SizeVolume volumeB)
+        public bool CompareVolume(SizeVolume volumeA, SizeVolume volumeB)
         {
             if (volumeA.Volume() > volumeB.Volume())
                 return true;
