270efbfd367e547ca3eae9c1309aaca7b879f12d
AusTLaiN <Chunga4anga@gmail.com>
2017-02-17 15:04:40 +0200

    KPSS_29

---
+++ KPSS_29/BoxCompressor.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApplication1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPSS_29
+{
+    public static class BoxCompressor
+    {
+        public static ISize GetSize(List<IBox> boxes)
+        {
+            ISize size = new Size();
+
+            foreach (var box in boxes)
+            {
+                size.Height += box.ISize.Height;
+
+                if (size.Length < box.ISize.Length)
+                    size.Length = box.ISize.Length;
+
+                if (size.Width < box.ISize.Width)
+                    size.Width = box.ISize.Width;
+            }
+
+            return size;
+        }
+    }
+}
