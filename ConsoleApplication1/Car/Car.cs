33592c550604cd65974effcaf10dc909a13b7906
student <student@K0606>
2017-02-17 16:28:17 +0200

    add  method CountPrice

--- ConsoleApplication1/Car/Car.cs
+++ ConsoleApplication1/Car/Car.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApplication1.Size;
+using ConsoleApplication1.Box;
 
 namespace ConsoleApplication1.Car
 {
@@ -34,9 +36,20 @@ namespace ConsoleApplication1.Car
             }
         }
 
-        public double CountPrice(List<IBox> Boxes)
+        public virtual double CountPrice(List<IBox> Boxes)
         {
-            throw new NotImplementedException();
+            var result_size = BoxCompressor.GetSize(Boxes);
+            var carsize = new Car();
+            var result = new SizeComparer();
+
+            if (result.Compare(result_size, carsize.ISize))
+            {
+                return 0;
+            }
+            else
+            {
+                return 5;
+            }
         }
     }
 }
